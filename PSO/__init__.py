e9cbee6bdd88ea1ffdb11629fe845cb3f7e861e5
dhinadhinesh007 <r.dhineshkumar2014@vit.ac.in>
2020-03-31 13:09:47 -0700

    PSO skeleton

---
+++ PSO/__init__.py
@@ -0,0 +1,2 @@
+from .particle import Particle
+from .swarm import Swarm
---
+++ PSO/swarm.py
@@ -0,0 +1,28 @@
+class Swarm:
+
+    def __init__(self):
+        # Input:
+        # No. of Particles
+        # No. of Iterations
+        pass
+
+    @staticmethod
+    def fitness_value():
+        # Input:
+        # Reference summaries
+        # Model Generated summaries
+        pass
+
+    def update_particles(self):
+        pass
+
+    def score_sentences(self):
+        pass
+
+    def generate_summaries(self):
+        pass
+
+    def train(self):
+        # Train for number of Iterations / Terminating condition is met.
+        pass
+
