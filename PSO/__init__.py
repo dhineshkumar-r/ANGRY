21cec052882bb6112bbc5088c4652b4f71a8c8a3
aayushshah96 <aayush1996@hotmail.com>
2020-04-04 18:43:14 -0700

    Added extract feature function to init

--- PSO/__init__.py
+++ PSO/__init__.py
@@ -1,2 +1,18 @@
 from .particle import Particle
 from .swarm import Swarm
+from FeatureGen import FeatureGen
+
+
+def extract_features(tokenized_doc):
+    """
+    Args:
+        tokenized_doc (List[List[str]]): Tokenized document
+
+    Returns:
+        features (Numpy.Array): N*D dimension Array of features
+    """
+    features_object = FeatureGen(tokenized_doc)
+    feature_vector = features_object.getfeaturevec(tokenized_doc)
+
+    return feature_vector
+
