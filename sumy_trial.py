123e68dd57e07e4169a65fe50ef2c7345948e0d1
aayushshah96 <aayush1996@hotmail.com>
2020-04-05 15:30:25 -0700

    Added rogue to textrank code

--- sumy_trial.py
+++ sumy_trial.py
@@ -1,12 +1,31 @@
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.text_rank import TextRankSummarizer
+import PSO
 import Utils
 
-parser = PlaintextParser.from_file("sample_data/sample_hindi_text.txt", Utils.Tokenizer())
+fn = "history_ncert_class10/chap_3.txt" # sys.argv[1]
+ref_dir_n = "history_ncert_class10/annotations/chapter3" # sys.argv[2]
+summary_length = 75
+
+parser = PlaintextParser.from_file(fn, Utils.Tokenizer())
+
+# load file
+document, ref_sum = Utils.load_documents(fn, ref_dir_n)
+
+processed_ref_sum = Utils.process_documents(ref_sum)
+ref_sum = PSO.join_docs(processed_ref_sum)
 
 summarizer = TextRankSummarizer()
 summarizer.stop_words = Utils.load_stop_words()
-summary = summarizer(parser.document, 2)
+summary = summarizer(parser.document, summary_length)
 
+p_sum1 = ""
 for sentence in summary:
-    print(sentence)
+    p_sum1 += str(sentence) + " "
+
+print("Final Rouge Score: ", Utils.calculate_rouge(p_sum1, ref_sum, 1))
+
+f = open("predicted_summary_textrank.txt", 'w', encoding='utf-8')
+for sentence in summary:
+    f.write(str(sentence) + '\n')
+f.close()
